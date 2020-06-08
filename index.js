43316f059429e9f51c610883c7d29bcd10ca320f
neetsh24 <tamrakarnitesh21@gmail.com>
2020-06-08 09:50:01 +0545

    Mongoose part 2

--- index.js
+++ index.js
@@ -8,12 +8,10 @@ const connect=mongoose.connect(url);
 connect.then((db)=>{
     console.log('Connection successful to server');
 
-    var newDish=Dishes({
+    Dishes.create({
         name:'Uthappizza',
         description:'test'
-    });
-
-    newDish.save()
+    })
         .then((dish)=>{
             console.log(dish);
 
